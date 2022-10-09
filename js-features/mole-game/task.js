21ef55e85be4ee27b98dd5d8aa8d4a18b68dda30
Pavel Chekalin <grokki91@gmail.com>
2022-10-09 15:53:19 +0300

    hw 1 - moleGame fix

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -3,12 +3,19 @@ let lost = document.getElementById('lost');
 let counterDead = 0;
 let counterLost = 0;
 
-
 let getHole = index => document.getElementById(`hole${index}`);
 
 for (let i = 1; i < 10; i++) {
     let hole = getHole(i);
     hole.onclick = function() {
+        if (hole.className.includes('hole_has-mole')) {
+            counterDead++;
+            dead.textContent = counterDead;
+        } else {
+            counterLost++;
+            lost.textContent = counterLost;
+        }
+
         if (counterDead > 9) {
             result();
             alert('Победа!');
@@ -17,14 +24,6 @@ for (let i = 1; i < 10; i++) {
         if (counterLost > 4) {
             result();
             alert('Вы проиграли!');          
-        }
-
-        if (hole.className.includes('hole_has-mole')) {
-            counterDead++;
-            dead.textContent = counterDead;
-        } else {
-            counterLost++;
-            lost.textContent = counterLost;
         }
     }
 }
