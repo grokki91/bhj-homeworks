8b28658c64560aeebc1ff0d579ff4c2db57e2c07
Pavel Chekalin <grokki91@gmail.com>
2022-10-22 23:11:19 +0300

    hw 4 - reveal 1

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -7,7 +7,7 @@ document.addEventListener('scroll', e => {
         let elementPosition = el.getBoundingClientRect().top;
 
         if (elementPosition < viewportHeight / 2) {
-            el.classList.add('reveal_active')
+            el.classList.add('reveal_active');
         }
     })
 })
