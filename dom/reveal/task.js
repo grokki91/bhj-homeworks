5de7ddd26539b099543c677e2b5f72ad56e2bb61
Pavel Chekalin <grokki91@gmail.com>
2022-10-23 20:44:04 +0300

    hw 4 - reveal fix

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -4,9 +4,9 @@ document.addEventListener('scroll', e => {
     let viewportHeight = window.innerHeight;
 
     reveals.forEach(el => {
-        let elementPosition = el.getBoundingClientRect().top;
+        let {top} = el.getBoundingClientRect();
 
-        if (elementPosition < viewportHeight / 2) {
+        if (top < viewportHeight / 2) {
             el.classList.add('reveal_active');
         }
     })
