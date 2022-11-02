b468ec3fe365277a7c79e0c5d0bf8504284189d1
Pavel Chekalin <grokki91@gmail.com>
2022-11-02 16:48:05 +0300

    hw 6 - tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,5 +1,4 @@
 let links = [...document.querySelectorAll('.has-tooltip')];
-let tooltips = document.querySelectorAll('.tooltip');
 let tooltip = document.querySelector('.tooltip');
 
 links.forEach(link => {
@@ -7,7 +6,6 @@ links.forEach(link => {
         e.preventDefault();
         let { top, left } = link.getBoundingClientRect();
         let title = link.getAttribute('title');
-        tooltips.forEach(el => el.classList.remove('tooltip_active'))
         tooltip.classList.toggle('tooltip_active');
         tooltip.textContent = title;
         tooltip.style.top = top + 20 + 'px';
