99c9eef0db3f930f2547328f1a6bee21c80cbf4f
Pavel Chekalin <grokki91@gmail.com>
2022-11-06 23:54:02 +0300

    hw 6 cart

---
+++ document-structure/cart/task.js
@@ -0,0 +1,81 @@
+let incBtn = document.querySelectorAll('.product__quantity-control_inc');
+let decBtn = document.querySelectorAll('.product__quantity-control_dec');
+let buyBtn = document.querySelectorAll('.product__add');
+let cart = document.querySelector('.cart__products');
+let title = document.querySelector('.cart__title');
+
+incBtn.forEach(el => {
+    el.addEventListener('click', e => {
+        let value = el.closest('.product__quantity-controls').querySelector('.product__quantity-value');
+        value.textContent++;
+    })
+})
+
+decBtn.forEach(el => {
+    el.addEventListener('click', e => {
+        let value = el.closest('.product__quantity-controls').querySelector('.product__quantity-value');
+        value.textContent > 1 ? value.textContent-- : 1;
+    })
+})
+
+buyBtn.forEach(el => {
+    el.addEventListener('click', e => {
+        let id = el.closest('.product').dataset.id;
+        let src = el.closest('.product').querySelector('img').getAttribute('src');
+        let value = el.closest('.product').querySelector('.product__quantity-value');
+        
+        title.textContent = `Корзина`;
+        cart.innerHTML += `
+        <div class="cart__product" data-id="${id}">
+            <img class="cart__product-image" src="${src}">
+            <div class="cart__product-close">X</div>
+            <div class="cart__product-count">${value.textContent}</div>
+        </div>`;
+
+        // задание с анимацией
+        animationImg(el, id);
+
+        [...document.querySelectorAll('.cart__product > .cart__product-close')].forEach(close => {
+            close.addEventListener('click', e => {
+                e.target.closest('.cart__product').remove();
+                if (document.querySelector('.cart__products').children.length === 0) {
+                    title.textContent = ``;
+                }
+            })
+        })
+    })
+})
+
+
+function animationImg(el, id) {
+    let step = 5;
+    let cloneImg = el.closest('.product').querySelector('img').cloneNode();
+    cloneImg.style.position = 'absolute';
+    let imgProductX = el.closest('.product').querySelector('img').getBoundingClientRect().top;
+    let imgProductY = el.closest('.product').querySelector('img').getBoundingClientRect().left;
+    let imgCartX;
+    let imgCartY;
+    [...document.querySelectorAll('.cart__product')].forEach(elem => {
+        if (elem.dataset.id === id) {
+            imgCartX = elem.querySelector('img').getBoundingClientRect().top;
+            imgCartY = elem.querySelector('img').getBoundingClientRect().left;
+        }
+    });
+
+    let differX = imgProductX - imgCartX;
+    let differY = imgCartY - imgProductY;
+
+    let timerId = setInterval(() => {
+        cloneImg.style.top = imgProductX - differX / step + 'px';
+        cloneImg.style.left = imgProductY + differY / step + 'px';
+        document.body.insertAdjacentElement('afterbegin', cloneImg)
+
+        if (parseInt(cloneImg.style.top) <= imgCartX) {
+            cloneImg.remove();
+            clearInterval(timerId);
+        }
+
+        step--;
+    }, 100)
+
+}
--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,16 +1,20 @@
 let input = document.getElementById('task__input');
 let taskList = document.getElementById('tasks__list');
+let arrList = [];
 
 input.addEventListener('keypress', event => {
     const { key } = event;
 
     if (key === 'Enter' && input.value !== '') {
-        console.log('dfdsf');
         taskList.innerHTML += `
         <div class="task">
             <div class="task__title">${input.value}</div>
             <a href="#" class="task__remove">&times;</a>
         </div>`;
+        let index = document.querySelectorAll('.task').length - 1;
+        arrList.push(document.querySelectorAll('.task')[index]);
+
+        localStorage.setItem('task', arrList);
 
         input.value = '';
         [...document.querySelectorAll('.task__remove')].forEach(btn => {
