5ad6213d65fba312a76e9330a9d63e49263f3f55
Pavel Chekalin <grokki91@gmail.com>
2022-11-13 18:13:45 +0300

    hw preloader fix

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,28 +1,55 @@
 let loader = document.getElementById('loader');
 let items = document.getElementById('items');
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/');
-xhr.send();
+if (localStorage.getItem('valute')) {
+    loader.classList.remove('loader_active');
+    items.innerHTML = JSON.parse(localStorage.getItem('valute')).map(item => `
+    <div class="item">
+        <div class="item__code">
+            ${item.code}
+        </div>
+        <div class="item__value">
+        ${item.value}
+        </div>
+        <div class="item__currency">
+        ${item.currency}
+        </div>
+    </div>
+`).join('');
+} 
 
-xhr.addEventListener('readystatechange', function() {
-    if (xhr.readyState === 4 ) {
-        loader.classList.remove('loader_active');
-        valuteList = JSON.parse(this.response).response.Valute;
+else {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/');
+    xhr.send();
     
-        for (key of Object.entries(valuteList)) {
-            items.innerHTML += `
-                <div class="item">
-                    <div class="item__code">
-                        ${key[1].CharCode}
-                    </div>
-                    <div class="item__value">
-                        ${key[1].Value}
-                    </div>
-                    <div class="item__currency">
-                        руб.
-                    </div>
-                </div>`;
-        }
-    } 
-})
+    xhr.addEventListener('readystatechange', function() {
+        if (xhr.readyState === 4 ) {
+            loader.classList.remove('loader_active');
+            valuteList = JSON.parse(this.response).response.Valute;
+        
+            for (key of Object.entries(valuteList)) {
+                items.innerHTML += `
+                    <div class="item">
+                        <div class="item__code">
+                            ${key[1].CharCode}
+                        </div>
+                        <div class="item__value">
+                            ${key[1].Value}
+                        </div>
+                        <div class="item__currency">
+                            руб.
+                        </div>
+                    </div>`;
+            }
+            let item = [...document.querySelectorAll('.item')];
+            let itemArray = item.map(el => ({
+                code : el.querySelector('.item__code').innerText,
+                value : el.querySelector('.item__value').innerText,
+                currency : el.querySelector('.item__currency').innerText
+            }))
+            
+            localStorage.setItem('valute', JSON.stringify(itemArray));
+        } 
+    })
+}
