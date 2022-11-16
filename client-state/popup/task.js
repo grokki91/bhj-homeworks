8e1982006574476808d5c0af6df64300d18262c9
Pavel Chekalin <grokki91@gmail.com>
2022-11-16 14:22:11 +0300

    hw popup fix

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,7 +1,7 @@
 const modal = document.querySelector('.modal');
 const closeBtn = document.querySelector('.modal__close');
 
-document.cookie ? modal.classList.remove('modal_active') : modal.classList.add('modal_active');
+document.cookie === 'modal=close' ? modal.classList.remove('modal_active') : modal.classList.add('modal_active');
 
 closeBtn.addEventListener('click', e => {
    if (e.target.closest('.modal')) {
